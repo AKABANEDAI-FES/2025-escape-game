ff0e0af09070d2d02ae4266d69805a171a2a4b8f
1f10230002 <s1F102300021@iniad.org>
2025-10-12 01:59:04 +0900

    画像が表示されない問題を修正

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // ← GitHub Pages向けに静的出力
+  output: "export",
+  
   images: {
-    unoptimized: true, // ← 画像最適化を無効化（next export対応）
+    unoptimized: true,
   },
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
-      },
-    },
-  },
-  basePath: process.env.BASE_PATH, // ← GitHub Pages用のベースパス（自動注入される）
-  assetPrefix: process.env.BASE_PATH, // ← 画像やCSSのパスも自動調整
-  env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.BASE_PATH, // ← クライアント側にも注入
-  },
+  
+  basePath: process.env.BASE_PATH,
+  assetPrefix: process.env.BASE_PATH,
 };
 
 export default nextConfig;
