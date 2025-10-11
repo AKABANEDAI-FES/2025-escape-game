f770a58ea2cda17856362d867f5699fb9299f145
1f10230002 <s1F102300021@iniad.org>
2025-10-11 18:12:26 +0900

    githubPagesを設定

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
+const repo = "2025-escape-game";
+const assetPrefix = process.env.NODE_ENV === "production" ? `/${repo}` : "";
+const basePath = process.env.NODE_ENV === "production" ? `/${repo}` : "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
   images: {
     unoptimized: true,
-  }
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 export default nextConfig;
